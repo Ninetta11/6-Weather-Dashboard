28a43a0489290f65195e4607f5c70847f9b4cfe0
Ninetta11 <65838273+Ninetta11@users.noreply.github.com>
2020-09-21 12:00:40 +1000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,49 +2,64 @@ var cityList = document.getElementById("search-history");
 var searchEl = document.getElementById("search");
 var searchInput = document.getElementById("search-entry");
 var currentForecast = document.getElementById("current");
-var futureForecast = document.getElementById('future');
-
-var cityHeading = document.createElement("h2");
-var weeklyForecastHeading = document.createElement("h2");
-var displayDate = document.createElement("h3");
-var displayIcon = document.createElement("img");
-var displayTemp = document.createElement("p");
-var displayHumidity = document.createElement("p");
-var displayWindSpeed = document.createElement("p");
-var displayUV = document.createElement("p");
+var futureForecast = document.getElementById("future");
 
 var openWeatherURL = "https://api.openweathermap.org/";
 var apiKey = "be7a39895621e97d4b83ace8f5bc938f";
-var searchHistory = [];
+var citiesSearched = [];
 var city = "";
+var lat = "";
+var lon = "";
+
 
 // pulls saved search history from local storage and displays 
 function displaySearchHistory(){
-    searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+    var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
     if (searchHistory){
         for (var i = 0; i < searchHistory.length; i++) {
             city = searchHistory[i];
             createList();
         };
+    citiesSearched = searchHistory;
     };
 };
     
 
-// 
+// searching open weather database for input
 function search(){
-    //clear previous searches???
-    getCurrentForecast();
-    //need to put check in here if its a valid entry and for if search has aready been executed//
-    saveSearch();
+    var queryURL = openWeatherURL + "data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey;   
+    $.ajax({
+        url: queryURL,  
+        method: "GET",
+        // if a valid entry, saving search
+        success: function () {
+            if (citiesSearched.indexOf(city) === -1){
+            saveSearch();      
+            };
+        },
+        // if not valid entry, asking user to resubmit query
+        error: function(jqXHR, textStatus, errorThrown) { 
+            if(jqXHR.status == 404 || errorThrown == 'Not Found') { 
+                errortext = document.createElement("h4");
+                errortext.textContent = "We could not find a city matching that name. Please try again.";
+                currentForecast.append(errortext);
+            }
+        }
+    // obtaining latitude and longitude for forecast query
+    }).then(function(response){
+        lat = response.coord.lat;
+        lon = response.coord.lon;
+        getForecast();
+    });
 };
 
 
 // adds city to search history
 function saveSearch(){
-    var citiesSearched = searchHistory;
     citiesSearched.push(city);
     // updates saved search history to local storage
     localStorage.setItem("searchHistory", JSON.stringify(citiesSearched));
+    // adds search to search bar aside
     createList();
 };
 
@@ -58,120 +73,105 @@ function createList(){
 };
 
 
-// searches for current forecast based on input
-function getCurrentForecast(){
-    var queryURL = openWeatherURL + "data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey;   
+// displays forecast results 
+function getForecast(){
+    // searching open weather database with latitude and longitude
+    var queryURL = openWeatherURL + "data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=metric&exclude=current,minutely,hourly&appid=" + apiKey;
     $.ajax({
         url: queryURL,  
         method: "GET"
     }).then(function(response){
-        var weatherCondition = response.weather.icon;
-        var tempCelsius = response.main.temp;
-        var humidity = response.main.humidity;
-        var windSpeed = response.wind.speed;
-        var lat = response.coord.lat;
-        var lon = response.coord.lon;
+        // for the six days of forecast
+        for (var i = 0; i < 6; i++){
+            // creating elements for display
+            var cityHeading = document.createElement("h2");
+            var displayDate = document.createElement("h5");
+            var displayIcon = document.createElement("img");
+            var displayTemp = document.createElement("p");
+            var displayHumidity = document.createElement("p");
+            var displayWindSpeed = document.createElement("p");
+            var displayUV = document.createElement("p");
+            var uvIndexRating = "";
+            var day = document.createElement('li');
+            day.setAttribute("class", "list-group-item list-group-item-info ");
 
-        // searches for UV index and sets attribute based on value returned
-        var uvIndex = getUVIndex(lat,lon);
-        if (uvIndex <= 2){
-            displayUV.setAttribute("class", "low");
-        }
-        else if (uvIndex > 2 && uvIndex <= 5){
-            displayUV.setAttribute("class", "moderate");
-        }
-        else if (uvIndex > 5 && uvIndex <= 7){
-            displayUV.setAttribute("class", "high");
-        }
-        else if (uvIndex > 7 && uvIndex <= 10){
-            displayUV.setAttribute("class", "veryHigh");
-        }
-        else if (uvIndex > 10) {
-            displayUV.setAttribute("class", "extreme");
+            // pulls relevant data from response
+            var date = response.daily[i].dt;
+            var weatherCondition = response.daily[i].weather[0].icon;
+            var tempCelsius = response.daily[i].temp.day;
+            var humidity = response.daily[i].humidity;
+            var windSpeed = response.daily[i].wind_speed;
+            var uvIndex = response.daily[i].uvi;
+            var timezone = response.timezone;
+
+            // if it's the current day of forecast
+            if (i === 0){
+                // setting colour of text based on rating according to UV index returned
+                if (uvIndex <= 2){
+                    uvIndexRating = "low";
+                }
+                else if (uvIndex > 2 && uvIndex <= 5){
+                    uvIndexRating = "moderate";
+                }
+                else if (uvIndex > 5 && uvIndex <= 7){
+                    uvIndexRating = "high";
+                }
+                else if (uvIndex > 7 && uvIndex <= 10){
+                    uvIndexRating = "very-high";
+                }
+                else {
+                    uvIndexRating = "extreme";
+                };
+                
+                // setting content for display
+                cityDisplay = timezone.split("/");
+                cityHeading.textContent = city.toUpperCase() + ", " + cityDisplay[0];
+                displayIcon.setAttribute("class", "main");
+                displayIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherCondition + "@2x.png");
+                displayDate.setAttribute("class", "main-date");
+                displayDate.textContent = moment.unix(date).format('dddd Do MMMM');
+                displayTemp.textContent = "Temperature: " + Math.round(tempCelsius) + "\u00B0C";
+                displayHumidity.textContent = "Humidity: " + humidity + "%";
+                displayWindSpeed.textContent = "Wind Speed: " + windSpeed + " MPH";
+                displayUV.innerHTML = 'UV Index: <span class="' + uvIndexRating + '">' + uvIndex + '</span>';
+
+                // displaying content
+                currentForecast.append(cityHeading, displayDate, displayIcon, displayTemp, displayHumidity, displayWindSpeed, displayUV);
+            }
+            else {
+                // for subsequent five days of forecast, setting content for display
+                displayDate.textContent = moment.unix(date).format('dddd');
+                displayIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherCondition + "@2x.png");
+                displayTemp.textContent = "Temp: " + Math.round(tempCelsius) + "\u00B0C";
+                displayHumidity.textContent = "Humidity: " + humidity + "%";
+                
+                // displaying content
+                day.append(displayDate, displayIcon, displayTemp, displayHumidity);
+                futureForecast.append(day);
+            };
         };
-        displayUV.textContent = "UV Index: " + uvIndex;
-
-        // displays results
-        cityHeading.textContent = city;
-        displayIcon.setAttribute("src", selectIcon(weatherCondition));
-        displayTemp.textContent = "Temperature: " + tempCelsius.Math.round() + " C";
-        displayHumidity.textContent = "Humidity: " + humidity + "%";
-        displayWindSpeed.textContent = "Wind Speed: " + windSpeed + " MPH";
-        currentForecast.append(cityHeading, displayDate, displayIcon, displayTemp, displayHumidity, displayWindSpeed, displayUV);
-        
-        getWeeklyForecast(lat,lon);
-    })
-};
-    
-
-// searches for 5 day forecast based on input
-function getWeeklyForecast(lat,lon){
-    weeklyForecastHeading = "5-Day Forecast";   
-    var date = moment().unix();
-    for (var i = 0; i < 6; i++){
-        date = date - 86400;
-        var queryURL = openWeatherURL + "data/2.5/onecall/timemachine?lat=" + lat + "&lon=" + lon + "&dt=" + date + "&units=metric&appid=" + apiKey;
-        $.ajax({
-            url: queryURL,  
-            method: "GET"
-        }).then(function(response){
-            console.log(response);
-
-            var weatherCondition = response.current.weather.icon;
-            var tempCelsius = response.current.temp;
-            var humidity = response.current.humidity;
-            
-            // displays results
-            displayDate.textContent = moment().unix(date).format('Do MM YYYY');
-            displayIcon.setAttribute("src", selectIcon(weatherCondition));
-            displayTemp.textContent = "Temperature: " + tempCelsius.Math.round() + " C";
-            displayHumidity.textContent = "Humidity: " + humidity + "%";
-            futureForecast.append(weeklyForecastHeading, displayDate, displayIcon, displayTemp, displayHumidity);
-        })
-    };
-};
-
-
-// gets related icon for weather conditions
-function selectIcon(x){
-    var icon = x;
-    var queryURL = openWeatherURL + "img/wn/" + icon + "@2x.png";
-        console.log(queryURL);
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        return response;
-    })
-};
-
-// gets UV Index based on input
-function getUVIndex(lat,lon){
-    var queryURL = openWeatherURL + "data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
-    console.log(queryURL);
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        var uvIndex = response.value;
-        return uvIndex;
-    })
+    });
 };
 
 
 displaySearchHistory();
 
+// when the user clicks within the search aside 
 searchEl.addEventListener('click', function(event){
+    // if the user clicked the search button, uses inputted value for search
     if (event.target.matches('button')){
-        city = searchInput.value;
+        city = searchInput.value.toUpperCase();
+            // clears displayed content
+        $(currentForecast).empty();
+        $(futureForecast).empty();
+        searchInput.value = "";
         search();
     }
+    // if the user clicked on a saved search button, uses that value for search
     else if (event.target.matches('.cities')){
-        city = event.target.textContent;
+        city = event.target.textContent.toUpperCase();
+        $(currentForecast).empty();
+        $(futureForecast).empty();
         search();
     }
 });
-
-
-
-
